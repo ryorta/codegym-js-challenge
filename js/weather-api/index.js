b3c71e6b5f4ad181c23fff8a4401b36331a535b9
ryorta <ryotabaske13@gmail.com>
2022-08-09 08:56:15 +0000

    インデントの整理 fetch処理の関数化 DOMContentLoadedイベントリスナを一つにまとめる placeSelect.options[0].selected = true;処理の消去 地名ヘッダに対応する都市名を表示

--- js/weather-api/index.js
+++ js/weather-api/index.js
@@ -1,44 +1,29 @@
+const doFetch = (cities) => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?appid=4b5774e9f3d2a07b84f0f2f88e486224&units=metric&lang=ja&q=' + cities)
+        .then((response) => response.json())
+        .then((data) => {
+            const name = document.getElementById('name');
+            name.innerHTML = data['name'];
+
+            const temp = document.getElementById('temp');
+            temp.innerHTML = data['main']['temp'] + '℃';
+
+            const humi = document.getElementById('humi');
+            humi.innerHTML = data['main']['humidity'] + '%';
+
+            const press = document.getElementById('press');
+            press.innerHTML = data['main']['pressure'] + 'hPa';
+
+            const situation = document.getElementById('situation');
+            situation.innerHTML = data['weather'][0]['description'];
+        });
+};
+
 window.addEventListener('DOMContentLoaded', () => {
-const placeSelect = document.querySelector('#place');
-    placeSelect.options[0].selected = true;
+    doFetch('london');
+    const placeSelect = document.querySelector('#place');
     placeSelect.addEventListener('change', function() {
-        const cities = event.currentTarget.value;
-       
-    fetch('https://api.openweathermap.org/data/2.5/weather?appid=4b5774e9f3d2a07b84f0f2f88e486224&units=metric&lang=ja&q='+cities)
-    .then(response => response.json())
-    .then(data => {
-
-        const temp = document.getElementById('temp');
-        temp.innerHTML = data['main']['temp'] + '℃';
-
-        const humi = document.getElementById('humi');
-        humi.innerHTML = data['main']['humidity'] + '%';
-
-        const press = document.getElementById('press');
-        press.innerHTML = data['main']['pressure'] + 'hPa';
-
-        const situation = document.getElementById('situation');
-        situation.innerHTML = data['weather'][0]['description'];
-    })
+        const c = event.currentTarget.value;
+        doFetch(c);
     });
 });
-
-  window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?appid=4b5774e9f3d2a07b84f0f2f88e486224&units=metric&lang=ja&q=london')
-    .then(response => response.json())
-    .then(data => {
-
-        const temp = document.getElementById('temp');
-        temp.innerHTML = data['main']['temp'] + '℃';
-
-        const humi = document.getElementById('humi');
-        humi.innerHTML = data['main']['humidity'] + '%';
-
-        const press = document.getElementById('press');
-        press.innerHTML = data['main']['pressure'] + 'hPa';
-
-        const situation = document.getElementById('situation');
-        situation.innerHTML = data['weather'][0]['description'];
-    })
-    
-    });
